f4d94a0f2f4ef9b7e65e9065b48f1105f33238ce
Eujeanne Kim <puzzlepcs1@gmail.com>
2021-11-09 09:57:03 +0900

    Loss option modified

--- main.py
+++ main.py
@@ -32,10 +32,12 @@ def train(model: BasicModel, optimizer: optim.Adam, dataset: BasicDataset):
 
         classification_loss = F.nll_loss(Z[train_idx], Y[train_idx])
         # acc = accuracy(output=Z[train_idx], labels=Y[train_idx])
+        loss = config["lambda"] * classification_loss
 
-        negatives = sample_negatives(dataset, config['num_negatives']).to(config["device"])
-        topology_loss = model.topology_loss(negatives)
-        loss = config["lambda"] * classification_loss + (1-config["lambda"]) * topology_loss
+        if config["lambda"] < 1:
+            negatives = sample_negatives(dataset, config['num_negatives']).to(config["device"])
+            topology_loss = model.topology_loss(negatives)
+            loss += (1-config["lambda"]) * topology_loss
 
         loss.backward()
         optimizer.step()
